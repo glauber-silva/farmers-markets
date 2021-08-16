2f175592aaefa3fca700101ab0b2739e9917dd6b
Glauber L. Da Silva <glauber.lucio.silva@gmail.com>
2021-08-15 21:16:05 -0300

    add App struct

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,21 @@ package main
 
 import "fmt"
 
+// App - A struct which contains things related to database
+type App struct {
+}
+
+// Run - set up the application
+func (app *App) Run() error {
+	fmt.Println("Setting up the APP")
+	return nil
+}
+
 func main() {
 	fmt.Println("Farmers Markets Setup Project")
+	app := App{}
+	if err := app.Run(); err != nil {
+		fmt.Println("Error starting up the server")
+		fmt.Println(err)
+	}
 }

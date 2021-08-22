1ddeb5feacf6514dbb991adcca3ff04340bd0d43
Glauber L. Da Silva <glauber.lucio.silva@gmail.com>
2021-08-22 16:50:44 -0300

    add some tests

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/glauber-silva/farmers-markets
 go 1.16
 
 require (
+	github.com/go-resty/resty/v2 v2.6.0 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/jinzhu/gorm v1.9.16 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
 )
---
+++ test/handler_test.go
@@ -0,0 +1,58 @@
+package test
+
+import (
+	"fmt"
+	"github.com/go-resty/resty/v2"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestHealthEndpoint(t *testing.T) {
+	fmt.Println("Running  E2E test for health check")
+
+	client := resty.New()
+	r, err := client.R().Get("http://localhost:8080/api/health")
+	if err != nil {
+		t.Fail()
+	}
+
+	assert.Equal(t, 200, r.StatusCode())
+}
+
+func TestGetMarkets(t *testing.T) {
+	client := resty.New()
+	r, err := client.R().Get("http://localhost:8080/api/market")
+	if err != nil {
+		t.Fail()
+	}
+
+	assert.Equal(t, 200, r.StatusCode())
+}
+
+func TestPostMarket(t *testing.T) {
+	client := resty.New()
+	r, err := client.R().
+		SetBody(
+			`{"Long": -46550164,
+			"Lat": -23558733,
+			"Setcens": "355030885000091",
+			"Areap": "3550308005040",
+			"Coddist": 87,
+			"Distrito": "VILA FORMOSA",
+			"Codsubpref": 26,
+			"Subprefe": "ARICANDUVA-FORMOSA-CARRAO",
+			"Regiao5": "Leste",
+			"Regiao8": "Leste 1",
+			"NomeFeira": "VILA FORMOSA",
+			"Registro": "7777-6",
+			"Logradouro": "RUA TESTE",
+			"Numero": "S/N",
+			"Bairro": "VL FORMOSA",
+			"Referencia": "TRAVESSA MEIA-MEIA"
+	}`).
+		Post("http://localhost:8080/api/market")
+
+	assert.NoError(t, err)
+	assert.Equal(t, 200, r.StatusCode())
+	// TODO: assert if is a json/dict
+}
